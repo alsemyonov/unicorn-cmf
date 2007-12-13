b4791f9ea2bc3eb626a8db9ce1bf2ab3913fb865
rotuka <rotuka@b1111e2b-3839-0410-8c04-63d5f90cb5e2>
2007-12-13 22:17:08 +0000

    Удален(а) файл/папка

--- trunk/Version.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-final class Unicorn_Version {
-    const VERSION = '0.1.0';
-    static function compareVersion($version) {
-        return version_compare($version, self::VERSION);
-    }
-}
